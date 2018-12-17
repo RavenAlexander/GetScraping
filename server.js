c8cc22506cd81438b4ad927e09a84e28d31508e9
ralexander <theravenalexander@gmail.com>
2018-12-16 19:48:13 -0700

    mongo scraper

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var mongoose = require("mongoose");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router = express.Router();
+require("./config/routes")(router);
+
+app.use(express.static("public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT);
+});
